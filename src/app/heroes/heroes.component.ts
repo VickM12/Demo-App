88182bb4e1b5f889c23685f935e0b3f000afbf29
Vick Moberg <vicktor.moberg@gmail.com>
2022-08-11 22:28:55 -0400

    fixed heroes component, added .selected css class binding in heroes html file

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import { Hero } from '../hero';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -9,7 +9,14 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  };
+  selectedHero?: Hero;
+
+
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+
 
   constructor() { }
 
@@ -17,4 +24,4 @@ export class HeroesComponent implements OnInit {
   }
 
   
-
+}
