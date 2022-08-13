392aa58779a300cbb2774ce681acac12864080c7
Vick Moberg <vicktor.moberg@gmail.com>
2022-08-13 19:39:59 -0400

    refactored app.get

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/Tour-of-Heroes'));
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname, '/dist', 'Tour-of-Heroes', '/index.html'));
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
 
 });
 
