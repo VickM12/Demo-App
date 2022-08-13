c65f51dfe24bea3223e358fe8a73d6b8e8c93bb4
Vick Moberg <vicktor.moberg@gmail.com>
2022-08-13 13:14:17 -0400

    app is now fully CRUD.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,34 +1,92 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
-})
+  providedIn: 'root'})
+
 export class HeroService {
 
   private heroesURL = 'api/heroes'; //URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type' : 'application/json' })
+  };
   
 constructor(
   private http: HttpClient,
-  private messageService: MessageService
-     
-){}
+  private messageService: MessageService){}
+
   // GET heroes from the server
-   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesURL)
-  }
+  /** GET heroes from the server */
+getHeroes(): Observable<Hero[]> {
+  return this.http.get<Hero[]>(this.heroesURL)
+    .pipe(
+      tap(_ => console.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+}
 
+  //Get Hero by id. Will 404 if id not found
   getHero(id: number): Observable<Hero> {
-    //No handling if Hero has id yet. Will be implemented
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url =`${this.heroesURL}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => console.log(`fetched hero id= ${id}`)),
+      catchError(this.handleError<Hero>(`getHero id =${id}`))
+    );
   }
 
-  
+  //PUT: Update the Hero on the server
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesURL, hero, this.httpOptions).pipe(
+      tap(_ => console.log(`updated hero id= ${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /* POST: add a new hero to the server*/ 
+addHero(hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesURL, hero, this.httpOptions).pipe(
+    tap((newHero: Hero) =>console.log(`added hero w/ id= ${newHero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  );
+}
+
+/** DELETE: Delete the hero from the server */
+deleteHero(id: number): Observable<Hero>{
+  const url =`${this.heroesURL}/${id}`;
+
+  return this.http.delete<Hero>(url, this.httpOptions).pipe(
+    tap(_ => console.log(`deleted hero id= ${id}`)),
+    catchError(this.handleError<Hero>('deleteHero'))
+  );
+}
+
+
+ 
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation= 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      //TODO: send error to remote logging infrastructure
+      console.error(error); //Log to console instead
+
+      //TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      //Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,6 +13,20 @@ export class HeroesComponent implements OnInit {
   
   heroes: Hero[] = [];
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {return; }
+    this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero)
+    });
+  }
+
+  delete(hero: Hero): void{
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
   constructor(private heroService: HeroService) {}
 
  ngOnInit(): void {
@@ -23,5 +37,6 @@ getHeroes(): void {
   this.heroService.getHeroes()
   .subscribe(heroes => this.heroes = heroes);
 }
+
   
 }
