ad86e02f3cf7d62539ea489a4a76776462c439d7
Vick Moberg <vicktor.moberg@gmail.com>
2022-08-13 19:46:04 -0400

    refactored res.sendFile again

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/Tour-of-Heroes'));
 
 app.get('/*', function(req,res){
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
+    res.sendFile(path.join(`${__dirname}/build/index.html`))
 
 });
 
