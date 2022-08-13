ab42159118a140504d567f7e66c8233ca2bfabba
Vick Moberg <vicktor.moberg@gmail.com>
2022-08-13 15:41:57 -0400

    turned path name into heroku app name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path= require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/tour-of-heroes'));
+app.use(express.static(__dirname + '/dist/tour-of-heroes-ngapp'));
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname +'dist/tour-of-heroes'));
+    res.sendFile(path.join(__dirname +'dist/tour-of-heroes-ngapp'));
 
 });
 
