87edff7312fd5c78a1a9e0c282a90cc4e149fffa
Vick Moberg <vicktor.moberg@gmail.com>
2022-08-13 07:22:42 -0400

    added hero service to pass information/data to components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +9,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
 
@@ -17,8 +18,11 @@ onSelect(hero: Hero): void {
 }
 
 
+getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
   }
