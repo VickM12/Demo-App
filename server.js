0ac82405e867ca0677ec8d3c70963ac097fbe634
Vick Moberg <vicktor.moberg@gmail.com>
2022-08-13 15:39:51 -0400

    changed path name to lowercase

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path= require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/Tour-of-Heroes'));
+app.use(express.static(__dirname + '/dist/tour-of-heroes'));
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname +'dist/Tour-of-Heroes'));
+    res.sendFile(path.join(__dirname +'dist/tour-of-heroes'));
 
 });
 
