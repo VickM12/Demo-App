be5a11d2942d31b252a91611f92e0fdd165858da
Vick Moberg <vicktor.moberg@gmail.com>
2022-08-13 12:02:28 -0400

    added in memory db and api. added .get() method for http requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -25,6 +26,14 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    HttpClientModule,
+
+    // The following module intercepts HTTP requests
+    // and returns SIMULATED server responses.
+    // Remove it when a real server is ready to receive requests. 
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -10,12 +11,16 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-constructor(private messageService: MessageService) { }
-
-  getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes')
-    return heroes;
+  private heroesURL = 'api/heroes'; //URL to web api
+  
+constructor(
+  private http: HttpClient,
+  private messageService: MessageService
+     
+){}
+  // GET heroes from the server
+   getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesURL)
   }
 
   getHero(id: number): Observable<Hero> {
